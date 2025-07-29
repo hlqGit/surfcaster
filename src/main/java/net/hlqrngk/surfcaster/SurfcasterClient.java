e97743dcc0f07704a1e232246ae30320ac656604
hlq <justinjoslin@proton.me>
2025-07-29 15:54:42 -0500

    Prepare mod for development

---
+++ src/main/java/net/hlqrngk/surfcaster/SurfcasterClient.java
@@ -0,0 +1,10 @@
+package net.hlqrngk.surfcaster;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurfcasterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
