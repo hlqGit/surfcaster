bb55159cae4d19a55c3147aa7a777f2534f62ed3
hlq <justinjoslin@proton.me>
2025-07-29 15:36:34 -0500

    Initial commit

---
+++ src/main/java/net/hlqrngk/surfcaster/SurfcasterDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hlqrngk.surfcaster;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SurfcasterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
